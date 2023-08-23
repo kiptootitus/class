9386f765977821d6eca96718375060414d8e5d20
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-08-23 22:37:53 +0300

    models

--- class/models.py
+++ class/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from django.utils import timezone
+from django.urls import reverse
+from django.db.models.deletion import CASCADE
 # Create your models here.  
+
+
+class Topic(models.Model):  
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
 class Room(models.Model):   
-    # host = models.CharField(max_length=50)  
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=200)
-    #participants = models.ManyToManyField(User, related_name='participants')
-    
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)   
     updated= models.DateTimeField(auto_now=True)    
     created= models.DateTimeField(auto_now_add=True)
     
