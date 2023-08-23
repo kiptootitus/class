721dc186547868e993608a26cc7d6730e142d4bc
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-08-23 22:40:59 +0300

    models

--- class/models.py
+++ class/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.urls import reverse
 from django.db.models.deletion import CASCADE
-# Create your models here.  
 
 
 class Topic(models.Model):  
