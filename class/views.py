694d26c2965b26472e080e993b5dd42bcf46b5a2
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-08-23 19:49:39 +0300

    env set up

--- class/views.py
+++ class/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+rooms = [
+    {'id': 1, 'name': '101', 'floor': 1, 'beds': 2},
+    {'id': 2, 'name': '201', 'floor': 2, 'beds': 1},
+    {'id': 3, 'name': '301', 'floor': 3, 'beds': 3},
+]
+
+def home(request):
+    return render(request, 'home.html', {'rooms': rooms})
+
+def room(request):
+    return render(request,'room.html')
