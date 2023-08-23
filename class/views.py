cff360d273017c76bbfc9dcaa2dfcf97fba5334a
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-08-23 22:17:56 +0300

    database and models

--- class/views.py
+++ class/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Room
+ 
 # Create your views here.
 rooms = [
     {'id': 1, 'name': '101', 'floor': 1, 'beds': 2},
@@ -8,6 +9,7 @@ rooms = [
 ]
 
 def home(request ):
+    room = Room.objects.all()
     context={'rooms':rooms}
     return render(request, 'class/home.html', context)
 
