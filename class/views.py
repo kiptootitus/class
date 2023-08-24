c943534b41a341dec71409ff61ce3267d2e2c420
Titus Kiptoo <68914236+kiptootitus@users.noreply.github.com>
2023-08-24 21:53:20 +0300

    vies

--- class/views.py
+++ class/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
 from .forms import RoomForm
+
 
 def home(request ):
     rooms = Room.objects.all()
@@ -15,5 +16,10 @@ def room(request, pk):
 
 def createRoom(request):
     form=RoomForm()
+    if request.method=='POST':
+        form=RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
     context={'form':form}
     return render(request, 'class/room_form.html', context)
